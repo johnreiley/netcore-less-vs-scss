bc59d801e5885aca4d6ef9ab9a5a1c1997f9ee9a
John Reiley <johnreiley@labstats.com>
2020-06-12 11:24:24 -0600

    Add project files.

---
+++ NetCoreLessVsScss/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NetCoreLessVsScss.Models;
+using System.IO;
+
+namespace NetCoreLessVsScss.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController( ILogger<HomeController> logger )
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			if (Request.Path.HasValue)
+			{
+				if (Request.Path.Value == "/scss")
+				{
+					return View( new HomeViewModel() { CssPath = "css/scss/style.css" } );
+				}
+				if ( Request.Path.Value == "/less" )
+				{
+					return View( new HomeViewModel() { CssPath = "css/less/style.css" } );
+				}
+			}
+
+			return View( new HomeViewModel() );
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache( Duration = 0, Location = ResponseCacheLocation.None, NoStore = true )]
+		public IActionResult Error()
+		{
+			return View( new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier } );
+		}
+	}
+}
---
+++ NetCoreLessVsScss/gulpfile.js
@@ -0,0 +1,19 @@
+﻿/// <binding BeforeBuild='build' />
+const { parallel, src, dest } = require('gulp'),
+	sass = require('gulp-sass'),
+	less = require('gulp-less');
+var baseFilepath = "./wwwroot/css/";
+
+function buildSass() {
+	return src(baseFilepath + 'scss/style.scss')
+		.pipe(sass())
+		.pipe(dest(baseFilepath + 'scss'));
+}
+
+function buildLess() {
+	return src(baseFilepath + 'less/style.less')
+		.pipe(less())
+		.pipe(dest(baseFilepath + 'less'));
+}
+
+exports.build = parallel(buildSass, buildLess);
